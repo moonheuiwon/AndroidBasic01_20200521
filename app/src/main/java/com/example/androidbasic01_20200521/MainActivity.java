a618784ea45236930a9d40da3b4046c032f6583b
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 11:23:28 +0900

    데이터 바인딩 활성화

--- app/src/main/java/com/example/androidbasic01_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic01_20200521/MainActivity.java
@@ -1,20 +1,33 @@
 package com.example.androidbasic01_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbasic01_20200521.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         setUpEvents();
         setValues();
     }
 //    이벤트 처리 코드 모아두는 곳
     void setUpEvents() {
+
+        binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                FirstActivity로 이동.
+            }
+        });
 
     }
 //    화면에 나와야할 데이터 세팅 코드 모아두는 곳
